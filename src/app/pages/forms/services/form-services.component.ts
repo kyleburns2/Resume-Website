05231aaf8d8f2faf44b55568285994845852c8e8
kyleburns2 <kyle.burns2@marist.edu>
2018-04-11 11:08:24 -0400

    Initial Commit

---
+++ src/app/pages/forms/services/form-services.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngx-form-services',
+  styleUrls: ['./form-services.component.scss'],
+  templateUrl: './form-services.component.html',
+})
+export class FormServicesComponent {
+
+}
